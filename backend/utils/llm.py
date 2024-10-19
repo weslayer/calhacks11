4efc2de7b7b27e4e148a7b20795ac7706414d733
NicholasLe04 <nicholasl.le224@gmail.com>
2024-10-19 02:44:49 -0700

    Add LLM function

---
+++ backend/utils/llm.py
@@ -0,0 +1,28 @@
+import os
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+
+client = Groq(
+    api_key=os.environ.get("GROQ_API_KEY")
+)
+
+def generate_response(message: str, prefill:str = "") -> str:
+    completion = client.chat.completions.create(
+        model="llama-3.2-90b-text-preview",
+        messages=[
+            {
+                "role": "system",
+                "content": message
+            },
+            {
+                "role": "user",
+                "content": prefill
+            }
+        ],
+        temperature=1,
+        max_tokens=1024,
+        top_p=1,
+    )
+    return completion.choices[0].message.content
