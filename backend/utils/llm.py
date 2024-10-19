3517218cb0d5fab9e3bc3b76f1c44e79dd1a77e9
NicholasLe04 <nicholasl.le224@gmail.com>
2024-10-19 10:34:09 -0700

    Add demographic generation

--- backend/utils/llm.py
+++ backend/utils/llm.py
@@ -22,7 +22,7 @@ def generate_response(message: str, prefill:str = "") -> str:
             }
         ],
         temperature=1,
-        max_tokens=1024,
+        max_tokens=2048,
         top_p=1,
     )
     return completion.choices[0].message.content
