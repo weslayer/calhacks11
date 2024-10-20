c370f5016d32c94ec0b783ac543cb91bcd4f5da7
Alex Bui <alexnbui@gmail.com>
2024-10-20 02:33:41 -0700

    add key switch to llm.py

--- backend/utils/llm.py
+++ backend/utils/llm.py
@@ -4,12 +4,24 @@ from groq import Groq
 
 load_dotenv()
 
-client = Groq(
-    api_key=os.environ.get("GROQ_API_KEY")
-)
+clients=[
+  Groq(api_key=os.environ.get("GROQ_API_KEY1")),
+  Groq(api_key=os.environ.get("GROQ_API_KEY2")),
+  Groq(api_key=os.environ.get("GROQ_API_KEY3")),
+  Groq(api_key=os.environ.get("GROQ_API_KEY4")),
+  Groq(api_key=os.environ.get("GROQ_API_KEY5"))
+]
+
+client_num = 0
+
+def get_client():
+    global client_num
+    client = clients[client_num]
+    client_num = (client_num + 1) % len(clients)
+    return client
 
 async def generate_response(system: str, user: str = "", model: str="llama-3.2-3b-preview") -> str:
-    completion = client.chat.completions.create(
+    completion = get_client().chat.completions.create(
         model=model,
         messages=[
             {
